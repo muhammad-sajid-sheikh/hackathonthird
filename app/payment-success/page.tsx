defc552a516d5760cb1050725a83b180031b5c4d
Muhammad Sajid Sheikh <sajjalsajid@gmail.com>
2025-02-08 00:27:11 +0500

    update5

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,16 +1,16 @@
 interface PaymentSuccessProps {
-    searchParams: Record<string, string>;
+    searchParams: { [key: string]: string | string[] | undefined };
   }
   
   export default function PaymentSuccess({ searchParams }: PaymentSuccessProps) {
-    const amount = searchParams.amount || "0"; // Default value to avoid undefined
+    const amount = typeof searchParams.amount === "string" ? searchParams.amount : "0"; 
   
     return (
-      <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
+      <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-black to-gray-600">
         <div className="mb-10">
           <h1 className="text-4xl font-extrabold mb-2">Thank You!</h1>
           <h2 className="text-2xl">You Successfully sent</h2>
-          <div className="bg-white p-2 rounded-md text-purple-500 mt-5 text-4xl font-bold">
+          <div className="bg-white p-2 rounded-md text-black mt-5 text-4xl font-bold">
             ${amount}
           </div>
         </div>
